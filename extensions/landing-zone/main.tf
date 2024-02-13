3aafb9fb1ef6e5a7459668dd0a9de7e5ae06e06a
rajatagarwal-ibm <108140212+rajatagarwal-ibm@users.noreply.github.com>
2024-02-13 15:21:41 +0000

    extensions (#3)

---
+++ extensions/landing-zone/main.tf
@@ -0,0 +1,20 @@
+##############################################################################
+# IBM WebSphere Liberty operator deployment on the OCP cluster
+##############################################################################
+
+data "ibm_container_cluster_config" "cluster_config" {
+  cluster_name_id = var.cluster_id
+  config_dir      = "${path.module}/kubeconfig"
+  endpoint_type   = var.cluster_config_endpoint_type != "default" ? var.cluster_config_endpoint_type : null # null represents default
+}
+
+module "websphere_liberty_operator" {
+  source                               = "../.."
+  cluster_id                           = var.cluster_id
+  add_ibm_operator_catalog             = var.add_ibm_operator_catalog
+  create_ws_liberty_operator_namespace = var.create_ibm_mq_operator_namespace
+  ws_liberty_operator_namespace        = var.ibm_mq_operator_namespace
+  ws_liberty_operator_target_namespace = var.ibm_mq_operator_target_namespace
+  cluster_config_endpoint_type         = var.cluster_config_endpoint_type
+  operator_helm_release_namespace      = var.operator_helm_release_namespace
+}
---
+++ extensions/landing-zone/variables.tf
@@ -0,0 +1,46 @@
+variable "cluster_id" {
+  type        = string
+  description = "Id of the target IBM Cloud OpenShift Cluster."
+  nullable    = false
+}
+
+variable "operator_helm_release_namespace" {
+  type        = string
+  description = "Namespace to deploy the helm releases. Default to ibm-mq-operator."
+  default     = "ibm-mq-operator"
+}
+
+variable "add_ibm_operator_catalog" {
+  type        = bool
+  description = "Flag to configure the IBM Operator Catalog in the cluster before installing the IBM MQ Operator. Default is true."
+  default     = true
+}
+
+variable "create_ibm_mq_operator_namespace" {
+  type        = bool
+  description = "Flag to create the namespace where to deploy the IBM MQ Operator. Default to false."
+  default     = false
+}
+
+variable "ibm_mq_operator_namespace" {
+  type        = string
+  description = "Namespace to install the IBM MQ operator. Default to openshift-operators."
+  default     = "openshift-operators"
+}
+
+variable "ibm_mq_operator_target_namespace" {
+  type        = string
+  description = "Namespace to be watched by the IBM MQ Operator. Default to null (operator to watch all namespaces)."
+  default     = null
+}
+
+variable "cluster_config_endpoint_type" {
+  description = "Specify which type of endpoint to use for for cluster config access: 'default', 'private', 'vpe', 'link'. 'default' value will use the default endpoint of the cluster."
+  type        = string
+  default     = "default"
+  nullable    = false # use default if null is passed in
+  validation {
+    error_message = "Invalid Endpoint Type! Valid values are 'default', 'private', 'vpe', or 'link'"
+    condition     = contains(["default", "private", "vpe", "link"], var.cluster_config_endpoint_type)
+  }
+}
