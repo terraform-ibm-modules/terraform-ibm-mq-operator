6c1e06dab53b324a515cab332d2cd2a7ed855994
Rajat Agrawal <rajatatibm@gmail.com>
2024-04-12 10:41:37 +0100

    test: consume roks-quickstart flavor

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 module "landing_zone" {
-  source                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart//module?ref=v5.20.3"
+  source                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v5.20.3"
   region                 = var.region
   prefix                 = var.prefix
   tags                   = var.resource_tags
